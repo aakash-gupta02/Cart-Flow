3b6ff9512e1597d149fecc424993a8873b618fcb
Aakash <editsky597@gmail.com>
2025-10-29 23:02:15 +0530

    seller access

--- frontend/src/app/(dashboard)/seller/layout.js
+++ frontend/src/app/(dashboard)/seller/layout.js
@@ -2,8 +2,26 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
-export default function DashboardLayout({ children }) {
+export default function SellerLayout({ children }) {
+
+    const { user } = useAuthStore();
+    const router = useRouter();
+
+  useEffect(() => {
+    if (user && user.role !== "seller" && user.role !== "admin") {
+      router.replace("/login");
+      toast.error("You do not have permission.");
+    }
+  }, [user]);    
+
+
+
+
   return (
     <div className="[--header-height:calc(--spacing(14))]">
       <SidebarProvider className="flex flex-col">
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased selection:bg-indigo-600 selection:text-white`}
       >
         {children}
-        <Toaster/>
+        <Toaster position="top-right" />
       </body>
     </html>
   );
